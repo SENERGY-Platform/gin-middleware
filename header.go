03d9f0e7d98cbe43a1b2a3d61392f02e18ef46ab
y-du <42994541+y-du@users.noreply.github.com>
2023-05-05 12:06:01 +0200

    rename

--- header.go
+++ header.go
@@ -2,7 +2,7 @@ package gin_mw
 
 import "github.com/gin-gonic/gin"
 
-func NewStaticHeaderHandler(items map[string]string) gin.HandlerFunc {
+func StaticHeaderHandler(items map[string]string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		for key, val := range items {
 			c.Header(key, val)
