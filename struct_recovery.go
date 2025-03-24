6d7001ea46d98256e76abd986c3618e2fcffadfe
y-du <42994541+y-du@users.noreply.github.com>
2025-03-24 12:36:25 +0100

    add structured recovery handler

---
+++ struct_recovery.go
@@ -0,0 +1,61 @@
+package gin_mw
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+)
+
+const stackTraceAttrKey = "stack_trace"
+
+type recoveryStructuredLogger interface {
+	ErrorContext(ctx context.Context, msg string, args ...any)
+}
+
+func StructuredRecoveryHandler(structLogger recoveryStructuredLogger, handle gin.RecoveryFunc) gin.HandlerFunc {
+	return func(gc *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					var se *os.SyscallError
+					if errors.As(ne, &se) {
+						seStr := strings.ToLower(se.Error())
+						if strings.Contains(seStr, "broken pipe") ||
+							strings.Contains(seStr, "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+				if brokenPipe {
+					gc.Error(err.(error))
+					gc.Abort()
+				} else {
+					var stack []byte
+					buf := make([]byte, 1024)
+					for {
+						n := runtime.Stack(buf, false)
+						if n < len(buf) {
+							stack = buf[:n]
+							break
+						}
+						buf = make([]byte, 2*len(buf))
+					}
+					structLogger.ErrorContext(gc.Request.Context(), fmt.Sprintf("%s", err), stackTraceAttrKey, string(stack))
+					handle(gc, err)
+				}
+			}
+		}()
+		gc.Next()
+	}
+}
+
+func DefaultHandleRecovery(gc *gin.Context, _ any) {
+	gc.AbortWithStatus(http.StatusInternalServerError)
+}
