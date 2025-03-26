67c72c7c78688b0ffa565bb49374e3aed2b2f0df
y-du <42994541+y-du@users.noreply.github.com>
2025-03-26 14:18:27 +0100

    remove logger and return endpoints instead

--- routes.go
+++ routes.go
@@ -6,10 +6,6 @@ import (
 	"path"
 )
 
-type loggerItf interface {
-	Debug(v ...any)
-}
-
 type routerItf interface {
 	BasePath() string
 	Handle(httpMethod, relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
@@ -17,19 +13,18 @@ type routerItf interface {
 
 type Routes[T any] []func(a T) (m, p string, hf gin.HandlerFunc)
 
-func (r Routes[T]) Set(a T, router routerItf, logger loggerItf) error {
+func (r Routes[T]) Set(a T, router routerItf) ([][2]string, error) {
 	set := make(map[string]struct{})
+	var endpoints [][2]string
 	for _, route := range r {
 		m, p, hf := route(a)
 		key := m + router.BasePath() + p
 		if _, ok := set[key]; ok {
-			return errors.New("duplicate route: " + m + " " + path.Join(router.BasePath(), p))
+			return nil, errors.New("duplicate route: " + m + " " + path.Join(router.BasePath(), p))
 		}
 		set[key] = struct{}{}
 		router.Handle(m, p, hf)
-		if logger != nil {
-			logger.Debug("set route: " + m + " " + path.Join(router.BasePath(), p))
-		}
+		endpoints = append(endpoints, [2]string{m, path.Join(router.BasePath(), p)})
 	}
-	return nil
+	return endpoints, nil
 }
